2b9337f3f292d5b3e4e46a0cd275e6e0dffaba00
Thai Le <thailekha@yahoo.com>
2018-12-11 10:34:36 -0600

    menu for navigation

---
+++ coffeefrontend/Views/MenuPageItem.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace coffeefrontend.Views
+{
+    public class MenuPageItem
+    {
+        public string Title { get; set; }
+        public Type TargetType { get; set; }
+    }
+}
