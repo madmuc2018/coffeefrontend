b6e57710d2b6546670f0a505cbe61340c77f07a7
Thai Le <thailekha@yahoo.com>
2018-12-11 11:01:22 -0600

    init order pages

---
+++ coffeefrontend/Views/NewOrderPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace coffeefrontend.Views
+{
+    public partial class NewOrderPage : ContentPage
+    {
+        public NewOrderPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnPlaceOrderClicked(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ coffeefrontend/Views/UpdateOrderPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace coffeefrontend.Views
+{
+    public partial class UpdateOrderPage : ContentPage
+    {
+        public UpdateOrderPage()
+        {
+            InitializeComponent();
+        }
+        public void OnUpdateOrderClicked(object sender, EventArgs e)
+        {
+        }
+    }
+}
