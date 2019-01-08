a1dbbea8cc9daee8789431e0604ecdfced522186
barbaraabur <barbaraabur@gmail.com>
2019-01-08 10:02:49 -0600

    more coffee properties, add scrollview, button rename

--- coffeefrontend/Models/Order.cs
+++ coffeefrontend/Models/Order.cs
@@ -4,29 +4,47 @@ namespace coffeefrontend
     public class Order : IEquatable<Order>
     {
         public string id { get; set; }
-        public string type { get; set; }
+        public string producer { get; set; }
+        public string farm { get; set; }
+        public string elevation { get; set; }
+        public string variety { get; set; }
+        public string process { get; set; }
         public string quantity { get; set; }
-        public string from { get; set; }
+        public string qc { get; set; }
+        public string tastingNotes { get; set; }
         public string status { get; set; }
+        public string changedBy { get;}
+        public string changedAt { get; }
+
 
         public Order()
         { }
 
-        public Order(string id, string type, string quantity, string from, string status)
+        public Order(string id, string producer, string farm, string elevation, string variety, string process, string quantity, string qc, string tastingNotes, string status)
         {
             this.id = id;
-            this.type = type;
+            this.producer = producer;
+            this.farm = farm;
+            this.elevation = elevation;
+            this.variety = variety;
+            this.process = process;
             this.quantity = quantity;
-            this.from = from;
+            this.qc= qc;
+            this.tastingNotes = tastingNotes;
             this.status = status;
         }
 
         public bool Equals(Order other)
         {
             return id == other.id 
-                && type == other.type 
-                && quantity == other.quantity 
-                && from == other.from 
+                && producer == other.producer 
+                && farm == other.farm 
+                && elevation == other.elevation
+                && variety == other.variety
+                && process == other.process
+                && quantity == other.quantity
+                && qc == other.qc
+                && tastingNotes == other.tastingNotes
                 && status == other.status;
         }
     }
