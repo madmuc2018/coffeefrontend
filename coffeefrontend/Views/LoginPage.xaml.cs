0f499e84ad37a81ef85c719f878e002d66c026d3
Thai Le <thailekha@yahoo.com>
2019-01-21 23:24:47 -0600

    tabbed login register page

--- coffeefrontend/Views/LoginPage.xaml.cs
+++ coffeefrontend/Views/LoginPage.xaml.cs
@@ -2,25 +2,11 @@
 
 namespace coffeefrontend
 {
-    public partial class LoginPage : ContentPage
+    public partial class LoginPage : TabbedPage
     {
         public LoginPage()
         {
             InitializeComponent();
         }
-
-        //private (bool, string, string, string) getUsernameAndPassword() 
-        //{
-        //    string username = usernameEntry.Text;
-        //    string password = passwordEntry.Text;
-        //    string role = roleEntry.Text;
-
-        //    if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(role))
-        //    {
-        //        DisplayAlert("Alert", "Please input username and password", "Close");
-        //        return (false, "", "", "");
-        //    }
-        //    return (true,username,password,role);
-        //}
     }
 }
