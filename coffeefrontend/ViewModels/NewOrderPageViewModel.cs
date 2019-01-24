61a3db581da9f695cb287a5d96b6e17e68c86094
Thai Le <thailekha@yahoo.com>
2019-01-23 22:00:42 -0600

    prevent submitting order twice

--- coffeefrontend/ViewModels/NewOrderPageViewModel.cs
+++ coffeefrontend/ViewModels/NewOrderPageViewModel.cs
@@ -6,7 +6,7 @@ namespace coffeefrontend
 {
     public class NewOrderPageViewModel : BaseViewModel
     {
-        private Order order;
+        private Order preSubmit, order;
         public ICommand SubmitCommand { protected set; get; }
 
         public NewOrderPageViewModel()
@@ -14,12 +14,21 @@ namespace coffeefrontend
             order = new Order();
             SubmitCommand = new Command(async () =>
             {
+                if (preSubmit != null && preSubmit.Equals(order))
+                {
+                    await Application.Current.MainPage.DisplayAlert("Alert", "Order has been submitted", "Close");
+                    return;
+                }
                 order.status = "placed";
+
                 (string error, string result) = await App.Manager.AddOrderTask(Application.Current.Properties["coffee_token"].ToString(), order);
                 if (error != null)
                     await Application.Current.MainPage.DisplayAlert("Alert", "Cannot include order", "Close");
                 else
+                {
+                    preSubmit = order;
                     await Application.Current.MainPage.DisplayAlert("Result", "Order included", "Close");
+                }
             });
         }
 
