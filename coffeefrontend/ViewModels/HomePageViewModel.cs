86a7dab5b946f2769e0cd8e3df1df84451280fe1
Thai Le <thailekha@yahoo.com>
2019-03-19 14:48:25 -0600

    homepage filtering orders

--- coffeefrontend/ViewModels/HomePageViewModel.cs
+++ coffeefrontend/ViewModels/HomePageViewModel.cs
@@ -10,15 +10,76 @@ namespace coffeefrontend
         public ICommand ToGrantAccess { protected set; get; }
         public ICommand GenerateQRCode { protected set; get; }
         public ICommand GetHistory { get; }
-        public List<OrderResp> OrderResps { get; }
+        public ICommand FilterOrders { protected set; get; }
+        private string filterer;
+        private List<OrderResp> filteredOrderResps;
 
         public HomePageViewModel(List<OrderResp> orders, Command toUpdate, Command toGrantAccess, Command generateQRCode, Command getHistory)
         {
-            this.OrderResps = orders;
             ToUpdate = toUpdate;
             ToGrantAccess = toGrantAccess;
             GenerateQRCode = generateQRCode;
             GetHistory = getHistory;
+            filterer = "";
+            filteredOrderResps = new List<OrderResp>();
+            FilteredOrderResps = ConstructFilteredOrdersList(orders, filterer);
+
+            FilterOrders = new Command(() =>
+            {
+                FilteredOrderResps = ConstructFilteredOrdersList(orders, filterer);
+            });
+        }
+
+        private List<OrderResp> ConstructFilteredOrdersList(List<OrderResp> o, string f)
+        {
+            List<OrderResp> temp = new List<OrderResp>();
+
+            o.ForEach(x =>
+            {
+                if (f.Length == 0)
+                {
+                    temp.Add(x);
+                }
+                else
+                {
+                    if (x.data.id.ToLower().Contains(f.ToLower()))
+                    {
+                        temp.Add(x);
+                    }
+                }
+
+            }
+                );
+
+            return temp;
+        }
+
+        public string Filterer
+        {
+            get => filterer;
+            set
+            {
+                if (!filterer.Equals(value))
+                {
+                    filterer = value;
+                    if (FilterOrders.CanExecute(null))
+                        FilterOrders.Execute(null);
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public List<OrderResp> FilteredOrderResps
+        {
+            get => filteredOrderResps;
+            set
+            {
+                if (!filteredOrderResps.Equals(value))
+                {
+                    filteredOrderResps = value;
+                    OnPropertyChanged();
+                }
+            }
         }
     }
 }
